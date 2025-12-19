fde83176eec04f18df12c140895900c6f3ec7b8d
Pedro Rodrigues <pedro.rodrigues@supabase.io>
2025-12-19 17:22:05 +0000

    fix lint

--- examples/checkout-demo/vite.config.ts
+++ examples/checkout-demo/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { skybridge } from "skybridge/web";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), skybridge()],
